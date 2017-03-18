ebbded4af1b95d595a98f7beb67e2a17ee9e1f31
Murad <muradmagomedow@gmail.com>
2017-03-19 01:22:23 +0300

    rendering some rectangles

--- src/whiteTale/Tile.java
+++ src/whiteTale/Tile.java
@@ -9,7 +9,16 @@ public class Tile {
     public int animateY;
     public boolean black;
 
-    public boolean inTile(int x, int y){
-        return (x > this.)
+    public Tile(int x, int y, boolean black) {
+        this.x = x;
+        this.y = y;
+        this.black = black;
+
+    }
+
+    public boolean pointInTile(int x, int y){
+        int width = WhiteTale.TILE_WIDTH;
+        int height = WhiteTale.TILE_HEIGHT;
+        return x > this.x * width && x < this.x * width + width && y > this.y * height && y < this.y * height + height;
     }
 }
--- src/whiteTale/WhiteTale.java
+++ src/whiteTale/WhiteTale.java
@@ -4,15 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created on 18/03/2017.
  */
-public class WhiteTale implements ActionListener {
+public class WhiteTale implements ActionListener, MouseListener {
 
-    public final int TILE_WIDTH = 250, TILE_HEIGHT = 450;
+    public final static int TILE_WIDTH = 250, TILE_HEIGHT = 250;
 
     public static WhiteTale dttwt;
 
@@ -20,21 +23,49 @@ public class WhiteTale implements ActionListener {
 
     public Renderer renderer;
 
+    public Random random;
+
+    public int score, milSecDelay;
+
+    public boolean delay;
+
     public WhiteTale(){
 
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("White tale");
 
         Timer timer = new Timer(20, this);
 
+        tiles = new ArrayList<Tile>();
         renderer = new Renderer();
+        random = new Random();
 
         frame.setSize(TILE_WIDTH*3,TILE_HEIGHT*3);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        start();
         timer.start();
     }
 
+    public void start(){
+        for (int x = 0; x<3; x++){
+            for (int y = 0; y<4; y++){
+                boolean canBeBlack = true;
+                for (Tile tile : tiles){
+                    if (tile.y == y && tile.black){
+                        canBeBlack = false;
+                    }
+                }
+                if (!canBeBlack){
+                    tiles.add(new Tile(x,y, false));
+                }
+                else{
+                    tiles.add(new Tile(x,y, random.nextInt(3) == 0 || x == 2));
+                }
+
+            }
+        }
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -44,10 +75,10 @@ public class WhiteTale implements ActionListener {
 
     public void render(Graphics g){
         for (Tile tile : tiles){
-            g.setColor(Color.BLACK);
-            g.setColor(Color.BLACK);
-            g.drawRect(tile.x * TILE_WIDTH, tile.y * TILE_HEIGHT - tile.animateY, TILE_WIDTH, TILE_HEIGHT);
+            g.setColor(tile.black ? Color.BLACK: Color.WHITE);
             g.fillRect(tile.x * TILE_WIDTH, tile.y * TILE_HEIGHT - tile.animateY, TILE_WIDTH, TILE_HEIGHT);
+            g.setColor(tile.black ? Color.WHITE: Color.BLACK);
+            g.drawRect(tile.x * TILE_WIDTH+1, tile.y * TILE_HEIGHT - tile.animateY-1, TILE_WIDTH-1, TILE_HEIGHT-1);
         }
     }
 
@@ -60,4 +91,43 @@ public class WhiteTale implements ActionListener {
     public void repaint(Graphics g) {
     }
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        for (int i = 0; i < tiles.size(); i++){
+            Tile tile = tiles.get(i);
+
+            if (tile.pointInTile(e.getX(), e.getY())){
+                if (tile.black){
+                    tile.animateY -= TILE_HEIGHT;
+                    tile.y++;
+
+                    for
+
+                }
+                else {
+                    gameOver = true;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
